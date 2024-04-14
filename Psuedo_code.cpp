07339d57d44dc9c659d22d06fab66ae44f29015c
CHizzyEzu <155769930+CHizzyEzu@users.noreply.github.com>
2024-04-14 11:31:17 -0400

    Added more detailed and accurate documentation within psuedo code

--- Psuedo_code.cpp
+++ Psuedo_code.cpp
@@ -1,37 +1,47 @@
-// Write a program for arithmetic operations ending with a sentinel value of -1
+//  Write a program for arithmetic operations ending with a sentinel value of -1.
 
-// Start of with relevant libraries and create a terminal statement to prompt user input
+//  Start off with relevant libraries and create a terminal statement to prompt user input.
 
-// #include <iostream>
-// #include <fstream
-// using namespace std;
+//  #include <iostream>
+//  #include <fstream
+//  #include <limits>
+//  #include <string>
+//  using namespace std;
 
-// Create a cout statement that askks for user input and rejects charactern data
+//  Create a cout statement that asks for user input and rejects charactern data.
 
-// Make sure you are validating user input for values that are not associated with the problem or
-// using relevant keywords to match more complicated parameters
-
-// While the user is asked to input data the user will have the option to end the code by entering -1
-
-// Turn the user input into a variable that can be used within the calculation part of your program
-
-// Now prompt the user to choose an option within a menu you built 
-
-/* Make sure you are building a switch case algorithim that holds the information for each operation
-When that happens turn the choice into a variable and use that within the equation aspect of the code */
-
-// Once the user has inputed a choice prompt the user to either choose to calculate more values or calculate the arthemtic problem they built
-
-/* Contain the code for the switch case and its logical math within a while loop, display the menu options within the
-    the loop and the loop will keep track of how many times the user inputs data and output solution,
-    it will also be responsible for continuing the code with a prompt as long as options 1 - 5 are chosen
-    for each iteration of a choice being picked the code will increment its count for each various menu option chosen within 
-    each switch case
+/*  The file begins at the start of the main function and encompasses all the code used to run the program.
+    Use the ofstream function and write data to the file using the name the user gives in a string and + ".txt" at the end.
+    Make sure before your program begins you then open the file you named so all information is written to it.
+    To account for error in opening the file create an if statement that accounts for issues with the file opening.
 */
 
-// All information that pertains to the solution of the problem should be contained within a file that is named by the user
+//  Make sure you are validating user input for values that are not associated with the problem, or
+//  using relevant keywords to match more complicated parameters.
 
-// Create a string to accomplish the naming of the file and replace it with the usualy text needed to name the file
+/*  While the user is asked to input data the user will have the option to end the code by entering -1.
+    Create a character exclusion statement to validate user input and allow only numbers to be submitted for answers.
+*/
 
-// Your file should finally be inputted into a file of your choice (.txt file recommended) and results should show up in that file
+/*  Next turn the user input into a variable that can be used within the switch case aspect of your program.
+    Now prompt the user to choose an option within the menu you built. 
+    Make sure you are building a switch case algorithm that holds the information for each operation and 
+    keeps track of each time an arithmetic operation is used by incrementing the variable within the switch case. 
+    Lastly within case 4 make sure to add input validation for division by zero and give the user an error for it
+    and run the code again.
+*/
+
+//  Once the user has inputed a choice prompt the user to either choose to calculate more values, or calculate the arithmetic problem they built.
+
+/*  Contain the code for the switch case and its logical math within a while loop, display the menu options within the
+    loop as well. It will also be responsible for continuing the code with a prompt as long as options 1 - 5 are chosen.
+    Ensure input validation is added to make sure the user can only input options 1 - 5.
+*/
+
+/*  All information that pertains to the solution of the problem should be contained within a file that is named by the user.
+    Create a string to accomplish the naming of the file and replace it with the usualy text needed to name the file.
+    At the end of the program remember to close the file and end the program with return 0.
+*/
+
+ // That concludes the program
 
