acce81d2b0d5ba2b510328cc5046444477038b4c
CHizzyEzu <155769930+CHizzyEzu@users.noreply.github.com>
2024-04-13 22:53:23 -0400

    Arthmetic Calculation Psuedo Code and program

---
+++ Arithmetic_Operation.cpp
@@ -0,0 +1,111 @@
+// Creating a menu that gives the user a choice between multiple different arthmetic values 
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>  
+#include <limits>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string yourname; // Name the file with user name
+
+    // Initialize variables that hold arthmetic solutions
+    int additionCount = 0, subtractionCount = 0, multiplicationCount = 0, divisionCount = 0, moduloCount = 0, totalCount = 0;
+    int num1, num2, choice; // values user inputs
+
+    cout << "Enter your name: ";
+    getline(cin >> ws, yourname); 
+
+    ofstream outputFile; // file opens
+    outputFile.open(yourname + ".txt"); 
+
+    // Loop for user data 
+    while (true)
+    {
+        cout << "Enter first number (-1 to exit): ";
+        if(!(cin >> num1))
+        {
+            cout << "Invalid input. Please input a number." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+        if(num1 == -1) 
+            break;
+
+        cout << "Enter second number: ";
+        if(!(cin >> num2))
+        {
+            cout << "Invalid input. Please enter a number." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+
+        // Menu 
+        cout << " Choose an operation: \n";
+        cout << "1. Addition\n";
+        cout << "2. Subtraction\n";
+        cout << "3. Multiplication\n";
+        cout << "4. Division\n";
+        cout << "5 Modulo\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        // Menu arithmetic choices 
+        switch (choice)     
+        {
+            case 1:
+                cout << "Result: " << num1 + num2 << endl;
+                additionCount++;
+            break;
+
+            case 2:
+                cout << "Result: " << num1 - num2 << endl;
+                subtractionCount++;
+            break; 
+
+            case 3:
+                cout << "Result: " << num1 * num2 << endl;
+                multiplicationCount++;
+                break;
+
+            case 4:
+                if (num2 != 0)
+                {
+                    cout << "Result: " << num1 / num2 << endl;
+                }
+                else
+                {
+                    cout << " Error: Division by zero!" << endl;
+                }
+                divisionCount++;
+                break;
+
+            case 5: 
+                cout << "Result: " << num1 % num2 << endl;
+                moduloCount++;
+                break;
+
+            default: // User validation to input only the selcted options
+            cout << "Invalid choice. Please enter a number between 1 - 5." << endl;
+        }      
+
+        totalCount++; // Keeps track of choices made
+    }
+
+    // Output of the arthmetic chosen and the amount of times the options were used
+    cout << "Addition: " << additionCount << endl;
+    cout << "Subtraction: " << subtractionCount << endl;
+    cout << "Multiplication: " << multiplicationCount << endl;
+    cout << "Division: " << divisionCount << endl;
+    cout << "Modulo: " << moduloCount << endl;
+    cout << "Total: " << totalCount << endl;
+
+    outputFile.close();
+
+    return 0;
+}
---
+++ Psuedo_code.cpp
@@ -0,0 +1,37 @@
+// Write a program for arithmetic operations ending with a sentinel value of -1
+
+// Start of with relevant libraries and create a terminal statement to prompt user input
+
+// #include <iostream>
+// #include <fstream
+// using namespace std;
+
+// Create a cout statement that askks for user input and rejects charactern data
+
+// Make sure you are validating user input for values that are not associated with the problem or
+// using relevant keywords to match more complicated parameters
+
+// While the user is asked to input data the user will have the option to end the code by entering -1
+
+// Turn the user input into a variable that can be used within the calculation part of your program
+
+// Now prompt the user to choose an option within a menu you built 
+
+/* Make sure you are building a switch case algorithim that holds the information for each operation
+When that happens turn the choice into a variable and use that within the equation aspect of the code */
+
+// Once the user has inputed a choice prompt the user to either choose to calculate more values or calculate the arthemtic problem they built
+
+/* Contain the code for the switch case and its logical math within a while loop, display the menu options within the
+    the loop and the loop will keep track of how many times the user inputs data and output solution,
+    it will also be responsible for continuing the code with a prompt as long as options 1 - 5 are chosen
+    for each iteration of a choice being picked the code will increment its count for each various menu option chosen within 
+    each switch case
+*/
+
+// All information that pertains to the solution of the problem should be contained within a file that is named by the user
+
+// Create a string to accomplish the naming of the file and replace it with the usualy text needed to name the file
+
+// Your file should finally be inputted into a file of your choice (.txt file recommended) and results should show up in that file
+
